a8c55e9dadb1d2342235327f5c110a5af08d27d2
phen1234 <phen12341@gmail.com>
2026-08-24 20:08:57 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -340,7 +340,6 @@ app.get("/", (req, res) => {
     });
 });
 
-
 // ==========================
 // SERVER
 // ==========================
@@ -348,20 +347,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-
     console.log(`✅ Server running on port ${PORT}`);
-
-});
-
-
-// ==========================
-// SERVER
-// ==========================
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, "0.0.0.0", () => {
-
-    console.log(`✅ Server running on port ${PORT}`);
-
 });
