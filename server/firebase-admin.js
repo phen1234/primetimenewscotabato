f0fe442bff377a24e0c01b460f51a645d455ed76
phen1234 <phen12341@gmail.com>
2026-08-24 13:44:25 +0800

    Update firebase-admin.js

--- server/firebase-admin.js
+++ server/firebase-admin.js
@@ -2,13 +2,28 @@ import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
-const { default: serviceAccount } =
-    await import("./serviceAccountKey.json", {
-        with: { type: "json" }
+let credential;
+
+if (process.env.FIREBASE_PROJECT_ID) {
+
+    credential = cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
     });
 
+} else {
+
+    const { default: serviceAccount } =
+        await import("./serviceAccountKey.json", {
+            with: { type: "json" }
+        });
+
+    credential = cert(serviceAccount);
+}
+
 initializeApp({
-    credential: cert(serviceAccount)
+    credential
 });
 
 export const adminAuth = getAuth();
