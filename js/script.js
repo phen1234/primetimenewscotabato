ef75178e4182c118ccedf75077eca418ac0d7862
phen1234 <phen12341@gmail.com>
2026-09-04 07:09:52 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import { db } from "./firebase.js"; 
 import { getVideoId } from "./youtube.js"; 
-import { collection, doc, getDoc, getDocs, query, orderBy, limit } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js"; 
+import { collection, doc, getDoc, getDocs, query, where, orderBy, limit } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js"; 
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js"; 
 
 async function loadNews() { 
