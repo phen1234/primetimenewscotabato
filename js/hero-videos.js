7eba447803a05783d7ab66bc8912948b1ded58b3
phen1234 <phen12341@gmail.com>
2026-09-06 14:52:48 +0800

    Update hero-videos.js

--- js/hero-videos.js
+++ js/hero-videos.js
@@ -63,8 +63,8 @@ async function loadVideos() {
 
 function startTicker() { 
     const container = heroVideos.parentElement; 
-    const itemHeight = 98; // height ng 1 video
-    const containerHeight = 336; // <-- DITO. FIXED 330 = 110 x 3
+    const itemHeight = 115; // height ng 1 video
+    const containerHeight = 460; // <-- DITO. FIXED 330 = 110 x 3
     const scrollHeight = container.scrollHeight; 
     
     if(scrollHeight <= containerHeight) return; 
